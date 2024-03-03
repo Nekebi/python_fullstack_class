54ef0f8d5665c4c287332cda31416ac9ad7d8795
Denis <Denis@MacBook-Air.local>
2024-03-03 21:04:00 +0300

    master

---
+++ Desktop/Python course/folder_1/first_file.py
@@ -0,0 +1,7 @@
+# Мой первый код на Python
+print ('Hello cats 1!')
+print ('Hello cats 2!')
+print ('Hello cats 3!')
+
+# Этот код работает как надо ! 
+# Автор Денис Казаков 
---
+++ Desktop/Python course/folder_1/second_file.py
@@ -0,0 +1,9 @@
+# This program prints Hello, dogs!
+print ('Hello, dogs 1')
+print ('Hello, dogs 2')
+print ('Hello, dogs 3')
+print ('Hello, dogs 4')
+print ('Hello, dogs 5')
+
+# Этот код работает исправно!
+# Автор разработчик - № 2 
